9ac749791fd50ea0e06cc611f7a79eb7c732994c
robreeves <robertpreeves@gmail.com>
2018-08-12 17:19:11 -0700

    comments

--- genopheno/models/model_data.py
+++ genopheno/models/model_data.py
@@ -1,10 +1,14 @@
 import random
 import pandas as pd
+from collections import namedtuple
+
+
+ModelData = namedtuple('ModelData', 'training testing')
 
 
 class ModelDataBuilder:
     """
-    A builder to mutate the preprocessed data for modeling. The phenotype dataframes are in the format:
+    A builder to mutate the preprocessed data for modeling. The input phenotype dataframes are in the format:
          Gene_info, user1, user2, ..., userN
     snp1,
     snp2,
@@ -13,6 +17,15 @@ class ModelDataBuilder:
 
     Where the Gene_info column contains the SNP metadata. All user columns contain the number of mutations that user
     has for the SNP, ranging from 0 to 2.
+
+    This splits the data frames into testing and training data, allows for any custom manipulation of the data frames and
+    formats the data frames into the final format required by modeling.
+
+          snp1, snp2, ..., snp3, phenotype
+    user1,
+    user2,
+    ...
+    user3
     """
     def __init__(self, phenotypes, data_split):
         self._data_split = data_split
@@ -56,7 +69,8 @@ class ModelDataBuilder:
     def apply_to_training(self, func):
         """
         Applies a function to each training dataframe
-        :param func: The function to apply. It should return the resulting phenotype dataframe
+        :param func: The function to apply. It should return the resulting phenotype dataframe.
+        The func takes two args, the phenotype string label and the dataframe.
         """
         for key in self._training.keys():
             self._training[key] = func(key, self._training[key])
@@ -64,7 +78,8 @@ class ModelDataBuilder:
     def apply_to_testing(self, func):
         """
         Applies a function to each testing dataframe
-        :param func: The function to apply. It should return the resulting phenotype dataframe
+        :param func: The function to apply. It should return the resulting phenotype dataframe.
+        The func takes two args, the phenotype string label and the dataframe.
         """
         for key in self._testing.keys():
             self._testing[key] = func(key, self._testing[key])
@@ -72,28 +87,51 @@ class ModelDataBuilder:
     def apply(self, func):
         """
         Applies a function to each training and testing dataframe
-        :param func: The function to apply. It should return the resulting phenotype dataframe
+        :param func: The function to apply. It should return the resulting phenotype dataframe.
+        The func takes two args, the phenotype string label and the dataframe.
         """
         self.apply_to_training(func)
         self.apply_to_testing(func)
 
     def reduce_training(self, reducer):
+        """
+        Applies a function to reduce the training data into a single value
+        :param reducer: The function that will reduce the phenotypes. It should return the reduced value.
+        The reducer takes one arg, a map of the training dataframes.
+        :return: The reducer function return value
+        """
         return reducer(self._training)
 
     def reduce_testing(self, reducer):
+        """
+        Applies a function to reduce the testing data into a single value
+        :param reducer: The function that will reduce the phenotypes. It should return the reduced value.
+        The reducer takes one arg, a map of the testing dataframes.
+        :return: The reducer function return value
+        """
         return reducer(self._testing)
 
     def build(self):
-        # todo drop non-user columns, transpose, add phenotype column
+        """
+        Builds the final data frames required for modeling
+        :return: An object containing the modeling training and testing data frames
+        """
+        # Finalize metadata related info in the data frames
         self.apply(self.__finalize_df)
 
-        # merge
+        # Merge the separate phenotypes into one data frame for training and one for testing
         training = self.reduce_training(self.__merge_phenos)
         testing = self.reduce_testing(self.__merge_phenos)
 
         return ModelData(training, testing)
 
-    def __finalize_df(self, pheno, pheno_df):
+    @staticmethod
+    def __finalize_df(pheno, pheno_df):
+        """
+        This finalizes the metadata in the dataframes. First, non-user columns are dropped (i.e. Gene_info).
+        Then the data frames are transposed so that users are the rows and SNPs are the values. A column for
+        phenotype is also added.
+        """
         # todo remove non-user columns
         columns_to_drop = filter(lambda x: not x.isdigit(), pheno_df.columns.values)
         pheno_df.drop(labels=columns_to_drop, axis=1, inplace=True)
@@ -104,12 +142,6 @@ class ModelDataBuilder:
 
         return transposed_data
 
-
-    def __merge_phenos(self, phenotypes):
+    @staticmethod
+    def __merge_phenos(phenotypes):
         return pd.concat(phenotypes.values())
-
-
-class ModelData:
-    def __init__(self, training, testing):
-        self.training = training
-        self.testing = testing
