be2c92ffb0e025db345092d0ca5529dd10de6f08
scott <tyung@dons.usfca.edu>
2019-02-24 13:02:39 -0800

    xgboost test

---
+++ genopheno/models/xgboostpytest.py
@@ -0,0 +1,24 @@
+from numpy import loadtxt
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+dataset = loadtxt('test.csv',delimiter =',')
+X = dataset[:,0:8]
+Y = dataset[:,8]
+
+seed = 7
+test_size = 0.33
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = test_size,random_state = seed)
+
+model = XGBClassifier()
+model.fit(X_train,Y_train)
+
+print(model)
+
+y_pred = model.predict(X_test)
+predictions = [round(value) for value in y_pred]
+
+accuracy = accuracy_score(Y_test,predictions)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
