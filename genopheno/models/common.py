6ec24f130210f9e64d805a7d9d622bb9602ff14a
scott <tyung@dons.usfca.edu>
2019-02-28 20:29:17 -0800

    XGBOOST

--- genopheno/models/common.py
+++ genopheno/models/common.py
@@ -52,11 +52,11 @@ def build_model(data_set, data_split, no_interactions, negative, model, cross_va
     # Do this after the test train split because while the ratio of each phenotype is the same in the split, different
     # rows are chosen based on which phenotype is assigned a 0 and 1. Do this after the split means consistent rows
     # will be selected regardless of which phenotype is assigned as the negative.
-    pheno_map = __pheno_to_binary(y_train, y_test, negative)
+    pheno_map = _pheno_to_binary(y_train, y_test, negative)
     model_config['pheno_map'] = pheno_map
 
     # Replace nan values
-    imputer, x_train, x_test = __impute_data(x_train, x_test)
+    imputer, x_train, x_test = _impute_data(x_train, x_test)
     model_config['imputer'] = imputer
 
     # print data counts
@@ -126,7 +126,7 @@ def __save_confusion_matrix(y_true, y_pred, output_dir, file_suffix):
         metrics_file.write(metrics)
 
 
-def __pheno_to_binary(y_train, y_test, negative):
+def _pheno_to_binary(y_train, y_test, negative):
     """
     Converts the phenotype labels to 0 and 1
     :param data_set: The feature matrix
@@ -154,7 +154,7 @@ def __pheno_to_binary(y_train, y_test, negative):
     return {0: negative, 1: positive}
 
 
-def __impute_data(x_train, x_test):
+def _impute_data(x_train, x_test):
     """
     Fills in the missing data. nan values will be replaced with the most frequent value for the feature
     :param x_train: The training data
--- genopheno/models/xgboostpytest.py
+++ genopheno/models/xgboostpytest.py
@@ -2,23 +2,41 @@ from numpy import loadtxt
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import Imputer
+import common
+import pandas as pd
 
-dataset = loadtxt('test.csv',delimiter =',')
-X = dataset[:,0:8]
-Y = dataset[:,8]
 
-seed = 7
-test_size = 0.33
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = test_size,random_state = seed)
 
-model = XGBClassifier()
-model.fit(X_train,Y_train)
+def xgboosting():
+    df = pd.read_csv('exported.csv')
 
-print(model)
+    print(df)
 
-y_pred = model.predict(X_test)
-predictions = [round(value) for value in y_pred]
+    #dataset = loadtxt('test.csv',delimiter =',')
 
-accuracy = accuracy_score(Y_test,predictions)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    X = df.drop(labels=['phenotype'], axis = 1)
+    Y = df['phenotype']
 
+    seed = 7
+    test_size = 0.1
+    X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = test_size,random_state = seed,stratify=Y)
+    imputer , X_train, X_test = common._impute_data(X_train, X_test)
+
+
+    model = XGBClassifier()
+    model.fit(X_train,Y_train)
+
+    print(model)
+
+    y_pred = model.predict(X_test)
+    predictions = [value for value in y_pred]
+    print (predictions)
+    print (Y_test)
+    accuracy = accuracy_score(Y_test,predictions)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+
+
+
+xgboosting()
